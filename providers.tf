6ac42812cba7995ffba795884d588ed0ceee4e93
Sai <docdoom12345@outlook.com>
2024-05-16 00:48:22 +0530

    updated providers.tf

--- providers.tf
+++ providers.tf
@@ -10,10 +10,5 @@ terraform {
 
 provider "azurerm" {
   features {}
-
-  client_id       = "d284b65d-5940-4e87-8aaa-74674314e75d"
-  client_secret   = ".JI8Q~aII7NLLUdJlrRKnVm7SfkgRSCAyAcUpbP7"
-  tenant_id       = "cea297cb-9bde-428d-9a6e-48fa9c582ed6"
-  subscription_id = "2a79f2da-f098-4c8a-8e2a-f426682b1eac"
 }
 /* */
